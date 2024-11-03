492230da9b77077028e373e887de35fe61d6dbf9
Gyeongjae Choi <def6488@gmail.com>
2024-11-03 01:24:18 +0000

    Sync recipes

--- packages/Cartopy/test_cartopy.py
+++ packages/Cartopy/test_cartopy.py
@@ -3,8 +3,6 @@ from functools import reduce
 import pytest
 from pytest_pyodide import run_in_pyodide
 
-from conftest import package_is_built
-
 DECORATORS = [
     pytest.mark.xfail_browsers(node="No supported matplotlib backends on node"),
     pytest.mark.skip_refcount_check,
@@ -27,28 +25,22 @@ def test_imports(selenium):
 
 
 @matplotlib_test_decorator
+@run_in_pyodide(packages=["Cartopy", "matplotlib", "pyodide-http"])
 def test_matplotlib(selenium):
-    if not package_is_built("pyodide-http"):
-        pytest.skip("pyodide-http is not built")
+    import io
 
-    @run_in_pyodide(packages=["Cartopy", "matplotlib", "pyodide-http"])
-    def run(selenium):
-        import io
+    import cartopy.crs as ccrs
+    import matplotlib.pyplot as plt
+    import pyodide_http
 
-        import cartopy.crs as ccrs
-        import matplotlib.pyplot as plt
-        import pyodide_http
+    pyodide_http.patch_all()
 
-        pyodide_http.patch_all()
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
 
-        ax = plt.axes(projection=ccrs.PlateCarree())
-        ax.coastlines()
+    fd = io.BytesIO()
+    plt.savefig(fd, format="svg")
 
-        fd = io.BytesIO()
-        plt.savefig(fd, format="svg")
-
-        content = fd.getvalue().decode("utf8")
-        assert len(content) > 100000
-        assert content.startswith("<?xml")
-
-    run(selenium)
+    content = fd.getvalue().decode("utf8")
+    assert len(content) > 100000
+    assert content.startswith("<?xml")
