457750cc8ec6183ce8933e3e364cce69b858638d
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-06-19 11:19:43 +0100

    fixup test

--- packages/narwhals/test_narwhals.py
+++ packages/narwhals/test_narwhals.py
@@ -5,22 +5,22 @@ from narwhals.utils import Version
 
 
 @run_in_pyodide(packages=["narwhals"])
-class MyDictDataFrame:
-    def __init__(self, data, version):
-        self._data = data
-        self._version = Version.MAIN
+def test_narwhals_from_native(selenium):
+    class MyDictDataFrame:
+        def __init__(self, data, version):
+            self._data = data
+            self._version = Version.MAIN
 
-    def __narwhals_dataframe__(self):
-        return self
+        def __narwhals_dataframe__(self):
+            return self
 
-    def _with_version(self, version):
-        return self.__class__(self._data, version)
+        def _with_version(self, version):
+            return self.__class__(self._data, version)
 
-    @property
-    def columns(self):
-        return list(self._data)
+        @property
+        def columns(self):
+            return list(self._data)
 
-
-assert nw.from_native(
-    MyDictDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, Version.MAIN)
-).columns == ["a", "b"]
+    assert nw.from_native(
+        MyDictDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, Version.MAIN)
+    ).columns == ["a", "b"]
