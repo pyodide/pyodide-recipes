d46949468f120989107374371f0f3c2b6faa8338
greateggsgreg <36009512+greateggsgreg@users.noreply.github.com>
2025-07-30 19:31:43 -0400

    Update test_certifi.py

--- packages/certifi/test_certifi.py
+++ packages/certifi/test_certifi.py
@@ -1,8 +1,7 @@
 from pytest_pyodide import run_in_pyodide
 
-
-@run_in_pyodide(packages=["cryptography"])
-def test_cryptography(selenium):
+@run_in_pyodide(packages=["certifi"])
+def test_certifi(selenium):
     import certifi
     import os
     assert os.path.exists(certifi.where())
