3cda95af9d5d3e9f83084506b22b503252babefd
Hood Chatham <roberthoodchatham@gmail.com>
2025-08-06 12:02:38 +0200

    Fix

--- packages/fastapi/test_fastapi.py
+++ packages/fastapi/test_fastapi.py
@@ -10,11 +10,11 @@ def test_fastapi(selenium):
         for key, value in file_contents:
             Path(key).write_text(value)
         import pytest
-        assert pytest.main(["fastapi_test.py"]) == 0
+        assert pytest.main(["fastapi_test_helper.py"]) == 0
 
     dir = Path(__file__).parent 
     file_contents = []
-    for name in ["asgi", "fastapi_test", "fastapi_test_app"]:
+    for name in ["asgi", "fastapi_test_helper", "fastapi_test_app"]:
         file = name + ".py"
         path = dir/file
         file_contents.append([file, path.read_text()])
