2b52fce142df1dcfc54d1c0ca299ba9b46000134
Hood Chatham <roberthoodchatham@gmail.com>
2025-08-07 10:31:10 +0200

    Fix again

--- packages/fastapi/test_fastapi.py
+++ packages/fastapi/test_fastapi.py
@@ -1,5 +1,7 @@
 from pytest_pyodide import run_in_pyodide
 from pathlib import Path
+import pytest
+
 
 @pytest.mark.xfail_browsers(firefox="Requires JSPI", safari="Requires JSPI")
 def test_fastapi(selenium_jspi):
@@ -11,12 +13,13 @@ def test_fastapi(selenium_jspi):
         for key, value in file_contents:
             Path(key).write_text(value)
         import pytest
+
         assert pytest.main(["fastapi_test_helper.py"]) == 0
 
-    dir = Path(__file__).parent 
+    dir = Path(__file__).parent
     file_contents = []
     for name in ["asgi", "fastapi_test_helper", "fastapi_test_app"]:
         file = name + ".py"
-        path = dir/file
+        path = dir / file
         file_contents.append([file, path.read_text()])
     inner(selenium, file_contents)
