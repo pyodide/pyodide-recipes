842cbdbbd4821f249efc1f9676edde745994220f
leehanjeong <rosy0111@naver.com>
2025-08-24 01:26:07 +0900

    Use dist_dir config variable instead of pyodide_root/dist

--- tools/copy_pyodide_runtime.sh
+++ tools/copy_pyodide_runtime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PACKAGE_BUILD_DIR=${1:-$(pwd)/dist}
-DIST_DIR=$(pyodide config get pyodide_root)/dist
+DIST_DIR=$(pyodide config get dist_dir)
 mkdir -p ${PACKAGE_BUILD_DIR}
 cp -r $DIST_DIR/pyodide.js $DIST_DIR/pyodide.mjs $DIST_DIR/pyodide.asm.js $DIST_DIR/pyodide.asm.wasm $DIST_DIR/python_stdlib.zip ${PACKAGE_BUILD_DIR}/
