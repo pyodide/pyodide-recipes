7f7b62079b3e728c3ac4e5065cd7ec9805d2f929
sikai zhang <matthew.szhang91@gmail.com>
2025-09-27 20:59:39 +0800

    test sklearn and fastcan

--- packages/fastcan/test_fastcan.py
+++ packages/fastcan/test_fastcan.py
@@ -6,7 +6,33 @@ from pytest_pyodide import run_in_pyodide
 @run_in_pyodide(packages=["fastcan"])
 def test_fastcan(selenium):
     from fastcan import FastCan
-    X = [[1, 0], [0, 1]]
-    y = [1, 0]
-    s = FastCan(verbose=0).fit(X, y).get_support()
-    assert (s == [True, False]).all()
+    from sklearn.datasets import make_classification
+    from sklearn.linear_model import LinearRegression
+    n_samples = 200
+    n_features = 20
+    n_classes = 8
+    n_informative = 5
+
+    X, y = make_classification(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_redundant=0,
+        n_repeated=0,
+        n_classes=n_classes,
+        n_clusters_per_class=1,
+        flip_y=0.0,
+        class_sep=10,
+        shuffle=False,
+        random_state=0,
+    )
+
+    reg = LinearRegression().fit(X[:, :n_informative], y)
+    gtruth_ssc = reg.score(X[:, :n_informative], y)
+
+    correlation_filter = FastCan(
+        n_features_to_select=n_informative,
+    )
+    correlation_filter.fit(X, y)
+    ssc = correlation_filter.scores_.sum()
+    assert ssc == pytest.approx(gtruth_ssc)
