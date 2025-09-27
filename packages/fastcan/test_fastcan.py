2c7cf5245c6e1c7339c9b08a04e06e685f87a3f2
sikai zhang <matthew.szhang91@gmail.com>
2025-09-27 19:17:59 +0800

    add selenium

--- packages/fastcan/test_fastcan.py
+++ packages/fastcan/test_fastcan.py
@@ -4,7 +4,7 @@ from pytest_pyodide import run_in_pyodide
 
 @pytest.mark.driver_timeout(60)
 @run_in_pyodide(packages=["fastcan"])
-def test_fastcan():
+def test_fastcan(selenium):
     from fastcan import FastCan
     X = [[1, 0], [0, 1]]
     y = [1, 0]
