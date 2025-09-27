c126b29ab35a39dd5ffe8ac3e97733e9517f24cd
sikai zhang <matthew.szhang91@gmail.com>
2025-09-27 18:38:35 +0800

    Add fastcan

---
+++ packages/fastcan/test_fastcan.py
@@ -0,0 +1,12 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@pytest.mark.driver_timeout(60)
+@run_in_pyodide(packages=["fastcan"])
+def test_fastcan():
+    from fastcan import FastCan
+    X = [[1, 0], [0, 1]]
+    y = [1, 0]
+    s = FastCan(verbose=0).fit(X, y).get_support()
+    assert (s == [True, False]).all()
