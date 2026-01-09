40ecc215bb410dc84849a8a928168fa373f59dfa
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-01-09 10:34:34 +0200

    Add Apache 2.0 license

--- packages/google-crc32c/test_google_crc32c.py
+++ packages/google-crc32c/test_google_crc32c.py
@@ -1,6 +1,22 @@
 import pytest
 from pytest_pyodide import run_in_pyodide
 
+# adopted from https://github.com/googleapis/python-crc32c/blob/9652503fb296eef101d84b9b631f454cc7c1d538/tests/test___init__.py
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 EMPTY = b""
 EMPTY_CRC = 0x00000000
 
@@ -70,7 +86,6 @@ _EXPECTED = [
 ]
 
 
-# adopted from https://github.com/googleapis/python-crc32c/blob/9652503fb296eef101d84b9b631f454cc7c1d538/tests/test___init__.py
 @pytest.mark.parametrize("chunk, expected", _EXPECTED)
 @run_in_pyodide(packages=["google-crc32c"])
 def test_checksum_value(selenium, chunk, expected):
