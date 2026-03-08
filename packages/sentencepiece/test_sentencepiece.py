2954e078a815540582a2bf4954c9893872b5c1d0
Bart Broere <mail@bartbroere.eu>
2026-03-08 08:53:25 +0000

    Add basic structure for sentencepiece (not tested yet)

---
+++ packages/sentencepiece/test_sentencepiece.py
@@ -0,0 +1,7 @@
+@run_in_pyodide(packages=["sentencepiece"])
+def test_simple_sentencepiece(selenium):
+    import sentencepiece
+
+    processor = sentencepiece.SentencePieceProcessor(model_file='test/test_model.model')
+    assert processor.encode('This is a test') == [284, 47, 11, 4, 15, 400]
+
