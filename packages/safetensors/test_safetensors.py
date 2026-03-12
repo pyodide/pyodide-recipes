858c2897b38b1b74250542f2456a6324d2db65c1
Bart Broere <mail@bartbroere.eu>
2026-03-12 05:44:31 +0000

    A preference thing

--- packages/safetensors/test_safetensors.py
+++ packages/safetensors/test_safetensors.py
@@ -3,12 +3,13 @@ from pytest_pyodide import run_in_pyodide
 
 @run_in_pyodide(packages=["safetensors", "numpy"])
 def test_safetensors(selenium):
-    import numpy as np
+    import numpy
     from safetensors.numpy import save, load
 
     tensors = {
-        "weight": np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32),
-        "bias": np.array([0.1, 0.2], dtype=np.float32),
+        "weight": numpy.array([[1.0, 2.0], [3.0, 4.0]], 
+                              dtype=numpy.float32),
+        "bias": numpy.array([0.1, 0.2], dtype=numpy.float32),
     }
 
     data = save(tensors)
@@ -16,5 +17,5 @@ def test_safetensors(selenium):
 
     loaded = load(data)
     assert set(loaded.keys()) == {"weight", "bias"}
-    np.testing.assert_array_equal(loaded["weight"], tensors["weight"])
-    np.testing.assert_array_equal(loaded["bias"], tensors["bias"])
+    numpy.testing.assert_array_equal(loaded["weight"], tensors["weight"])
+    numpy.testing.assert_array_equal(loaded["bias"], tensors["bias"])
