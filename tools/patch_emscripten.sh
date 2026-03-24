55e4e798848a717c4e7ed7f21136c6b12371e985
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-03-24 17:23:03 +0900

    Remove patches

--- tools/patch_emscripten.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EMSCRIPTEN_PATH="$(pyodide config get pyodide_root)/../../emsdk/upstream/emscripten"
-CURRENT_DIR="$(dirname "$(realpath "$0")")"
-
-echo "EMSCRIPTEN_PATH: $EMSCRIPTEN_PATH"
-cp "$CURRENT_DIR/cache.py" "$EMSCRIPTEN_PATH/tools/cache.py"
-cp "$CURRENT_DIR/system_libs.py" "$EMSCRIPTEN_PATH/tools/system_libs.py"
