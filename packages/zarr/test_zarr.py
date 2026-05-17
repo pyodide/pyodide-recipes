05e8217a836e00398de7d3a623d7a9648b5a3ae6
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-18 00:15:44 +0530

    It is `require_array` now

--- packages/zarr/test_zarr.py
+++ packages/zarr/test_zarr.py
@@ -58,7 +58,7 @@ def test_zarr_sync_wasm(selenium):
     # If sync() is broken on WASM this will raise RuntimeError before any IO
     store = zarr.storage.MemoryStore()
     root = zarr.open_group(store=store, mode="w")
-    arr = root.require_dataset("data", shape=(100,), chunks=(10,), dtype="f4")
+    arr = root.require_array("data", shape=(100,), chunks=(10,), dtype="f4")
     arr[:] = np.ones(100, dtype="f4")
     np.testing.assert_array_equal(arr[:], np.ones(100, dtype="f4"))
 
@@ -68,8 +68,9 @@ def test_zarr_local_store_wasm(selenium):
     import numpy as np
     import zarr
 
+    # exercises the exclusive-write path in LocalStore on Emscripten
     store = zarr.storage.LocalStore("/tmp/test_local.zarr")
     root = zarr.open_group(store=store, mode="w")
-    arr = root.require_dataset("x", shape=(50,), chunks=(10,), dtype="i4")
+    arr = root.require_array("x", shape=(50,), chunks=(10,), dtype="i4")
     arr[:] = np.arange(50)
     np.testing.assert_array_equal(arr[:], np.arange(50))
