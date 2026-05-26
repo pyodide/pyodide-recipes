ca60f03a541ee6cf5cb9fcfa1688b077f028f5de
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-26 19:08:34 +0530

    `test_cython_blas.+complex` now passing

--- packages/scipy/scipy-conftest.py
+++ packages/scipy/scipy-conftest.py
@@ -92,7 +92,6 @@ tests_to_mark = [
     ),
     # scipy/linalg tests
     ("test_blas.+test_complex_dotu", skip, todo_signature_mismatch_msg),
-    ("test_cython_blas.+complex", skip, todo_signature_mismatch_msg),
     ("test_cython_abi.py::test_cython_blas_abi_stability", xfail, todo_signature_mismatch_msg),
     ("test_cython_abi.py::test_cython_lapack_abi_stability", xfail, todo_signature_mismatch_msg),
     # scipy/ndimage/tests
