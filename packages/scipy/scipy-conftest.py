355c15bc74f1eb5c879b3646cda0e2e41ff11abe
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-26 19:23:46 +0530

    `test_complex_dotu` changed to `test_dotu`, passes

--- packages/scipy/scipy-conftest.py
+++ packages/scipy/scipy-conftest.py
@@ -91,7 +91,6 @@ tests_to_mark = [
         thread_msg,
     ),
     # scipy/linalg tests
-    ("test_blas.+test_complex_dotu", skip, todo_signature_mismatch_msg),
     ("test_cython_abi.py::test_cython_blas_abi_stability", xfail, todo_signature_mismatch_msg),
     ("test_cython_abi.py::test_cython_lapack_abi_stability", xfail, todo_signature_mismatch_msg),
     # scipy/ndimage/tests
