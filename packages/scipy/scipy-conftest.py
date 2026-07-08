b0356c3bb4756a085db05e779294233895b033ac
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-07-08 12:27:37 +0530

    Schur tests are fixed!

--- packages/scipy/scipy-conftest.py
+++ packages/scipy/scipy-conftest.py
@@ -88,20 +88,6 @@ tests_to_mark = [
     # scipy/linalg tests
     ("test_cython_abi.py::test_cython_blas_abi_stability", xfail, todo_signature_mismatch_msg),
     ("test_cython_abi.py::test_cython_lapack_abi_stability", xfail, todo_signature_mismatch_msg),
-    ("test_decomp.py::TestSchur", skip, todo_callback_signature_msg),
-    ("test_batch.py.*test_matmat\\[.*(sqrtm|signm)", skip, todo_callback_signature_msg),
-    ("test_batch.py.*test_funm", skip, todo_callback_signature_msg),
-    ("test_batch.py.*test_logm\\[", skip, todo_callback_signature_msg),
-    ("test_batch.py.*test_fractional_matrix_power", skip, todo_callback_signature_msg),
-    ("test_batch.py.*test_schur_lu\\[.*schur", skip, todo_callback_signature_msg),
-    ("test_matfuncs.py::TestExpM.test_logm_consistency", skip, todo_callback_signature_msg),
-    ("test_matfuncs.py::TestSignM", skip, todo_callback_signature_msg),
-    ("test_matfuncs.py::TestLogM", skip, todo_callback_signature_msg),
-    ("test_matfuncs.py::TestSqrtM", skip, todo_callback_signature_msg),
-    ("test_matfuncs.py::TestFractionalMatrixPower", skip, todo_callback_signature_msg),
-    ("test_decomp.py::TestQZ", skip, todo_callback_signature_msg),
-    ("test_decomp.py::TestOrdQZ", skip, todo_callback_signature_msg),
-    ("test_decomp.py::TestOverwrite::test_schur", skip, todo_callback_signature_msg),
     # TODO investigate: fatal crash, does not look like the callback issue.
     ("test_hyp2f1.py::TestHyp2f1.test_region4\\[hyp2f1_test_case14\\]", skip, "TODO investigate hyp2f1 region4 case14 crash"),
     # scipy/ndimage/tests
